0203acb841defde9779b21fba6d139410cf9e459
kxxsh93 <gusac@naver.com>
2020-07-20 11:59:58 +0900

    index, login, logout 페이지

--- human_wea/urls.py
+++ human_wea/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from nalsiwoori import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),    
+    path('logout/', views.logout, name='logout'),        
 ]
--- nalsiwoori/views.py
+++ nalsiwoori/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    pass
+    return render(request, 'nalsiwoori/index.html')
+    
 def login(request):
+    return render(request, 'nalsiwoori/login.html')
+
+def logout(request):
     pass
