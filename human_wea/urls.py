263ff96c900ab91a9c4a86e9612a29aa718a4efe
kxxsh93 <gusac@naver.com>
2020-07-20 15:16:41 +0900

    게시판 기능추가중

--- human_wea/urls.py
+++ human_wea/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),    
     path('logout/', views.logout, name='logout'),        
+    path('board/', views.board, name='board'),        
 ]
--- nalsiwoori/views.py
+++ nalsiwoori/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from .models import *
 
 def index(request):
     return render(request, 'nalsiwoori/index.html')
@@ -10,3 +11,12 @@ def login(request):
 
 def logout(request):
     pass
+
+def board(request):
+    sel_list = Selection.objects.order_by('pub_date')[:5]
+    html = ''
+
+    for s in sel_list:
+        html += s.state + '<br>'
+    return render(request, 'nalsiwoori/board.html', {'list': sel_list})
+    # return HttpResponse(html)
