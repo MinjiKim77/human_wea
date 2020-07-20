e9b6f2e19061018900606ac35a7a090e04d6c48f
Eul <shdmf89@gmail.com>
2020-07-20 16:52:28 +0900

    login 커밋

--- nalsiwoori/views.py
+++ nalsiwoori/views.py
@@ -7,16 +7,11 @@ def index(request):
     return render(request, 'nalsiwoori/home.html')
     
 def login(request):
-<<<<<<< HEAD
     user_id = request.POST.get('user_id')
     user_pwd = request.POST.get('user_pwd')
     print(user_id,user_pwd)
     request.session['user_id'] = user_id
-    return HttpResponse("로그인완료")
     return render(request, 'nalsiwoori/login.html')
-=======
-    return render(request, 'nalsiwoori/home.html')
->>>>>>> 21e229ee6dc7ea20b7e922f114b318dce347ff35
 
 def logout(request):
     pass
