6ae1f7f92cefb7806e9b8ccbdda8f60f5896cdfe
Eul <shdmf89@gmail.com>
2020-07-21 15:08:41 +0900

    로그인화면_1

--- human_wea/urls.py
+++ human_wea/urls.py
@@ -5,7 +5,8 @@ from nalsiwoori import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
-    path('login/', views.login, name='login'),    
+    path('signup/', views.signup, name='signup'), 
+    path('login/', views.login, name='login'),   
     path('logout/', views.logout, name='logout'),        
     path('course/', views.course, name='course'),        
     path('log_wea/', views.log_wea, name='log_wea'),        
