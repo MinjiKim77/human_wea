696027825077b867ecc4a87f6f87c0c5e423e247
Minji Kim <minzzzy222@gmail.com>
2020-07-21 16:27:07 +0900

    수정

--- human_wea/urls.py
+++ human_wea/urls.py
@@ -5,12 +5,8 @@ from nalsiwoori import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
-<<<<<<< HEAD
     path('signup/', views.signup, name='signup'), 
     path('login/', views.login, name='login'),   
-=======
-    # path('login/', views.login, name='login'),    
->>>>>>> c50ccb78f04b5be7801aa553f760ab2802dbfed8
     path('logout/', views.logout, name='logout'),        
     path('course/', views.course, name='course'),        
     path('log_wea/', views.log_wea, name='log_wea'),        
